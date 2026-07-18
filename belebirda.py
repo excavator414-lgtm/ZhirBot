85813569cfc1f9aefeca3c183075bf16530fbe5a
Excavator <popkabobra69@mail.ru>
2026-07-18 21:10:12 +0500

    изменения

--- belebirda.py
+++ belebirda.py
@@ -1,7 +1,9 @@
 import telebot
+from telebot import types
 from config import BOT_TOKEN
 
 bot = telebot.TeleBot(BOT_TOKEN)
+
 
 @bot.message_handler(commands=['GitHub'])
 def site(message):
@@ -18,9 +20,17 @@ def main(message):
 
 bot.message_handler(content_types=['photo'])
 def get_photo(message):
-    bot.reply_to(message, "Ага фото, круто")
+    markup = types.InlineKeyboardMarkup()
+    btn1 = types.InlineKeyboardMarkup('Съесть фото', callback_data='delete')
+    markup.row(btn1)
 
+    markup.add(types.InlineKeyboardMarkup('Съесть фото', callback_data='delete'))
+    bot.reply_to(message, "Ага фото, круто", reply_markup=markup)
 
+@bot.callback_query_handler(func=lambda callback:True)
+def callback_message(callback):
+    if callback.data == "delete":
+        bot.delete_message(callback.message.chat.id, callback.message.message_id -1)
 
 @bot.message_handler()
 def info(message):
