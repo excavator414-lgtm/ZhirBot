e8fa7a3669814df9966aef8c557b796771c49988
nonelop <morlksaq@gmail.com>
2026-07-18 17:55:51 +0700

    я тебе пофиксил вроде

--- belebirda.py
+++ belebirda.py
@@ -5,15 +5,15 @@ bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(commands=['GitHub'])
 def site(message):
-    bot.send_message('GitHub создателя бота по ссылке:', '\n https://github.com/excavator414-lgtm')
+    bot.send_message(message.chat.id, 'GitHub создателя бота по ссылке: \n https://github.com/excavator414-lgtm')
 
 @bot.message_handler(commands=['start', 'Hello'])
 def main(message):
     bot.send_message(message.chat.id, f'Привет,{message.from_user.first_name}! Я так называемый прототип будущих ботов от @Katft!')
 
 @bot.message_handler(commands=['Help'])
-def main(message):
-    bot.send_message(message.chat.id, f'Весь этот бот, это так называемый прототип работающий на серверах Render ','\nСозданный бот относится к @Katft', '<b>Созданный бот относится к</b> <u>@Katft</u>', parse_mode="html")
+def help(message):
+    bot.send_message(message.chat.id, f'Весь этот бот, это так называемый прототип работающий на серверах Render \n<b>Созданный бот относится к</b> <u>@Katft</u>', parse_mode="html")
 
 
 
