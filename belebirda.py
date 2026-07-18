ce612ec71280b71d34b99fa7b5cf65fca7a1f6a9
Excavator <popkabobra69@mail.ru>
2026-07-18 15:52:29 +0500

    изменение работы кода

--- belebirda.py
+++ belebirda.py
@@ -5,7 +5,7 @@ bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(commands=['GitHub'])
 def site(message):
-    bot.send_message('GitHub создателя бота по ссылке:', '\n https://github.com/excavator414-lgtm')
+    bot.send_message('GitHub создателя бота по ссылке: https://github.com/excavator414-lgtm')
 
 @bot.message_handler(commands=['start', 'Hello'])
 def main(message):
@@ -13,14 +13,14 @@ def main(message):
 
 @bot.message_handler(commands=['Help'])
 def main(message):
-    bot.send_message(message.chat.id, f'Весь этот бот, это так называемый прототип работающий на серверах Render ','\nСозданный бот относится к @Katft', '<b>Созданный бот относится к</b> <u>@Katft</u>', parse_mode="html")
+    bot.send_message(message.chat.id, f'<b>Созданный бот относится к</b> <u>@Katft</u>', parse_mode="html")
 
 
 
 @bot.message_handler()
 def info(message):
     if message.text.lower() == 'привет':
-        bot.send_message(message.chat.id, f'Привет,{message.from_user.first_name}! Я так называемый прототип будущих ботов от @Katft!')
+        bot.send_message(message.chat.id, f'Привет,{message.from_user.first_name} ! Я так называемый прототип будущих ботов от @Katft!')
     elif message.text.lower() == 'id':
         bot.reply_to(message, f'Ваш ID: {message.from_user.id}')
 
