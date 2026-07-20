1ef5f3bf71d0e3c56ce40ff507178c6413563b06
Excavator <popkabobra69@mail.ru>
2026-07-20 22:44:56 +0500

    измененно

--- belebirda.py
+++ belebirda.py
@@ -24,13 +24,20 @@ def get_photo(message):
     markup = types.InlineKeyboardMarkup()
     btn1 = types.InlineKeyboardButton('Съесть фото', callback_data='delete')
     markup.row(btn1)
+    btn2 = types.InlineKeyboardButton('Изменить текст', callback_data='edit')
+    markup.row(btn2)
     bot.reply_to(message, "Ага фото, круто", reply_markup=markup)
 
-@bot.callback_query_handler(func=lambda callback:True)
+
+
+@bot.callback_query_handler(func=lambda callback: True)
 def callback_message(callback):
     if callback.data == "delete":
         bot.delete_message(callback.message.chat.id, callback.message.message_id)
         bot.delete_message(callback.message.chat.id, callback.message.reply_to_message.message_id)
+    elif callback.data == "edit":
+        bot.edit_message_text('Текст изменён вроде', callback.message.chat.id, callback.message.message_id)
+
 # отправка фотографий под вопросом
 
 
